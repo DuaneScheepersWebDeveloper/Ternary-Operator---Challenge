ea0603ef4b415e1892c06cb64f8d1fb668f0c22d
Duane Scheepers <98160374+DuaneScheepersWebDeveloper@users.noreply.github.com>
2023-07-13 14:34:38 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Ternary_Operator___Challenge
+{
+    internal class Program
+    {
+        public static bool CheckTemperatureIsInteger(string input)
+        {
+            int number;
+            bool isInteger = int.TryParse(input, out number);
+            return isInteger;
+        }
+        static void Main(string[] args)
+        {
+            string temperatureMessage;
+            Console.Write("Please enter a temperature:");
+            string userInput = Console.ReadLine();
+            bool isInputInteger = CheckTemperatureIsInteger(userInput);
+            if (isInputInteger)
+            {
+                int checkedInput = Convert.ToInt32(userInput);
+                temperatureMessage = (checkedInput <= 15) ? "it is too cold here"
+                    : (checkedInput >= 16 && checkedInput <= 28) ? "It is ok" : "It is hot here";
+                Console.WriteLine(temperatureMessage);
+            }
+            else
+            {
+                Console.WriteLine("Please enter a valid temperature");
+            }
+            Console.Read();
+        }
+    }
+}
